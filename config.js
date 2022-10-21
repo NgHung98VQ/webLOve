96a5e387c6423d6d0a2fc95b61496dd49981f9d8
unknown <sharkilybk@gmail.com>
2022-10-21 22:06:57 +0700

    Hung

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello anhhhh yêu dấu của emmmmm!",
+  introDesc: `Trái đất vốn lạ thường\n
+    Mà sao anh cứ đi nhầm đường\n
+    Lạc vào tim em lẻ loi\n
+    Đằng sau chữ yêu đây là thương`,
+  btnIntro: "hihi",
+  title: "Phải chăng em đã chê ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng em đã chạy ngay từ lúc thấy nụ cười ấy ",
+  btnNo: "Vẫn cứ là iu emmm <33",
+  btnYes: "Không, em bị đin à >.<",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại không yêu anh <3",
+  btnReply: "Gửi cho emmmmm <3",
+  reply: "Chê nhaaaaaaaaa!<33333333",
+  mess: "Anh biết mà 🥰. Anh không xứng đáng có được em nhaaaaa! 😘😘",
+  messDesc: "Anh sẽ né em đến tận cùng thời gian nhé babiii :3",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://m.me/nen.goi.la.tung", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
