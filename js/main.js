c619a95ce8d3d2a2b28b668d5aa13ee58d5f081e
unknown <sharkilybk@gmail.com>
2022-10-21 22:28:44 +0700

    hung5

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // process bar
   var audio = new Audio("sound/sound.mp3");
-    audio.play();
+  audio.play();
   setTimeout(function () {
     firstQuestion();
     $(".spinner").fadeOut();
